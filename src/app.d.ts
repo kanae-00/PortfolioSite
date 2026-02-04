60299d25102a80d9d8de36792f023d1dbe742f3c
mk <work.kanae0@gmail.com>
2026-02-04 16:47:57 +0900

    update: Enhance type definitions for Svelte components and add module declarations for icon components

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,34 @@
-/// <reference types="@sveltejs/kit" />
-
+// See https://svelte.dev/docs/kit/types#app.d.ts
+// for information about these interfaces
+import type { Component } from 'svelte';
 declare global {
   namespace App {
     // interface Error {}
     // interface Locals {}
     // interface PageData {}
+    // interface PageState {}
     // interface Platform {}
   }
+}
+
+declare module '*.svelte' {
+  const component: Component<any>;
+  export default component;
+}
+
+declare module '$lib/component/icon/*.svelte' {
+  const component: Component<any>;
+  export default component;
+}
+
+declare module '$lib/component/icon/PeopleIcon.svelte' {
+  const component: Component<any>;
+  export default component;
+}
+
+declare module '$lib/component/icon/watchIcon.svelte' {
+  const component: Component<any>;
+  export default component;
 }
 
 export {};
